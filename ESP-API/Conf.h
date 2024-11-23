4797605ffbbef25a20088b9059103ec308edc4ad
Jonattan Sousa <jonattan.sousa@gmail.com>
2024-11-23 12:39:33 -0300

    Removed Conf.h from the tracking

--- ESP-API/Conf.h
+++
@@ -1,7 +0,0 @@
-// const char* ssid = "Watson";
-// const char* password = "jon@2050";
-// const String api_url = "http://laser-striped-flag.glitch.me";
-
-#define SSID "wireless_name"
-#define PWD "wireless_password"
-#define API "api_end"
